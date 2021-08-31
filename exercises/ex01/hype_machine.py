7333bf079b6e1508124c21ea04a9db5a7cc00bbe
Stella Manske <stellam@ad.unc.edu>
2021-08-31 12:53:20 -0400

    Progree on Exercise 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,9 @@
-# TODO: Write docstring here
-
-# TODO: Initialize __author__ variable here
+"""Hype Machine!"""
+ 
+__author__ = "730395784"
 
 # TODO: Implement your program logic here
+name: str = input("What is your name? ")
+print("You are so awesome, " + name)
+print(name + ", did you know you are going to pass this assignment?")
+print("Hello there, " + name + ", you are amazing!")
---
+++ exercises/ex01/relational_operators.py
@@ -0,0 +1,12 @@
+"""Relational Operators!"""
+
+__author__ = "730395784"
+
+left: str = input( "Left-hand side: ")
+right: str = input( "Right-hand side: ")
+left_integer: int = int(left)
+right_integer: int = int(right)
+print(left + " < " + right + " is " + str(left_integer < right_integer))
+print(left + " >= " + right + " is " + str(left_integer >= right_integer))
+print(left + " == " + right + " is " + str(left_integer == right_integer))
+print(left + " != " + right + " is " + str(left_integer != right_integer))
